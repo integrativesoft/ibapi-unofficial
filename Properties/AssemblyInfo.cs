201773fdc7169090299592b5ea32ddd03610fbfe
Pablo Carbonell <pablocar80@gmail.com>
2026-04-07 22:18:17 -0500

    remove garbage

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -2,16 +2,15 @@
  * and conditions of the IB API Non-Commercial License or the IB API Commercial License, as applicable. */
 
 using System.Reflection;
-using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CSharpAPI")]
+// [assembly: AssemblyTitle("CSharpAPI")]
 [assembly: AssemblyDescription("CSharpAPI")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Timber Hill (Europe) AG")]
-[assembly: AssemblyProduct("CSharpAPI")]
+// [assembly: AssemblyConfiguration("")]
+// [assembly: AssemblyCompany("Timber Hill (Europe) AG")]
+// [assembly: AssemblyProduct("CSharpAPI")]
 [assembly: AssemblyCopyright("Copyright (C) Timber Hill (Europe) AG 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -19,10 +18,10 @@ using System.Runtime.InteropServices;
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+// [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("074D7616-B13E-4351-A35F-0B673E3213BD")]
+// [assembly: Guid("074D7616-B13E-4351-A35F-0B673E3213BD")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -34,5 +33,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("10.37.02.0")]
-[assembly: AssemblyFileVersion("10.37.02.0")]
+// [assembly: AssemblyVersion("10.37.02.0")]
+// [assembly: AssemblyFileVersion("10.37.02.0")]
